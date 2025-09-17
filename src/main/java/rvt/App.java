b43579fa98e5e368a0ec4a1de67b96cf681b79f1
Emīls Penesis <A240733EP@rvt.lv>
2025-09-17 09:40:12 +0300

    Avarage of three numbers (2 pusgrupa)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -7,9 +7,11 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the first num: ");
-        int num = Integer.valueOf(scanner.nextLine());
+        double num = Integer.valueOf(scanner.nextLine());
         System.out.print("Enter the second num: ");
-        int num_2 = Integer.valueOf(scanner.nextLine());
-        System.out.println(String.valueOf(num) + " + " + String.valueOf(num_2) + " = " + (num + num_2));
+        double num_2 = Integer.valueOf(scanner.nextLine());
+        System.out.print("Enter the third num: ");
+        double num_3 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Avarage of three nums = " + ((num + num_2 + num_3) / 3));
     }
 }
