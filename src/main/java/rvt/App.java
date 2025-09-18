adb895f76a999138da7772f0752a00120e168f99
Emīls Penesis <A240733EP@rvt.lv>
2025-09-18 13:23:20 +0300

    Odd or Even

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -6,33 +6,14 @@ public class App {
 public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 
-System.out.println("Ieraksti punktus:");
-int punkti = Integer.valueOf(scanner.nextLine());
-scanner.close();
+System.out.println("Iedod numuru:");
+int numurs = Integer.valueOf(scanner.nextLine());
 
-if (punkti < 0) {
-System.out.println("Impossible!");
-}
-else if (punkti < 50) {
-System.out.println("failed");
-}
-else if (punkti < 60) {
-System.out.println("1");
-}
-else if (punkti < 70) {
-System.out.println("2");
-}
-else if (punkti < 80) {
-System.out.println("3");
-}
-else if (punkti < 90) {
-System.out.println("4");
-}
-else if (punkti < 100) {
-System.out.println("5");
+if (numurs % 2 == 0) {
+System.out.println("Number " + numurs + " is even");
 }
 else {
-System.out.println("Incredible");
+System.out.println("Number" + numurs + " is odd");
 }
 
 }
