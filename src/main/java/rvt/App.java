937f8408ea1093acfdd43aa0fd3b7092fb89e751
Emīls Penesis <A240733EP@rvt.lv>
2025-09-23 07:28:44 +0300

    Izveidoju jaunu failu "Sandbox", lai varētu testēt javas kodu. Kā arī izpildīju gift tax uzdevumu.

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,20 +1,36 @@
 package rvt;
-
 import java.util.Scanner;
 
-public class App {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
+    public class App {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of the gift: ");
+        double gift = Double.valueOf(scanner.nextLine());
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        }
+        else {
+            System.out.print("Tax: ");
+        }
+        if (gift >= 5000 && gift <= 25000) {
+            System.out.println(100 + (gift - 5000) * 0.08);
+        }
+        else if (gift > 25000 && gift <= 55000) {
+            System.out.print(1700 + (gift - 25000) * 0.10);
+        }
+        else if (gift > 55000 && gift <= 200000) {
+            System.out.print(4700 + (gift - 55000) * 0.12);
+        }
+        else if (gift > 200000 && gift <= 1000000) {
+            System.out.print(22100 + (gift - 200000) * 0.15);
+        }
+        else if (gift > 1000000) {
+            System.out.print(142100 + (gift - 1000000) * 0.17);
+        }
 
-System.out.println("Iedod numuru:");
-int numurs = Integer.valueOf(scanner.nextLine());
 
-if (numurs % 2 == 0) {
-System.out.println("Number " + numurs + " is even");
-}
-else {
-System.out.println("Number" + numurs + " is odd");
-}
 
-}
+        scanner.close();
+
+    }
 }
