8f91269d2cf07ad59b8f40bcfecc4e3ff3d4a673
Emīls Penesis <A240733EP@rvt.lv>
2025-09-24 09:42:03 +0300

    Delikateses

--- src/main/java/rvt/Sandbox.java
+++ src/main/java/rvt/Sandbox.java
@@ -2,14 +2,6 @@ package rvt;
 
 public class Sandbox {
     public static void main(String[] args) {
-        int x = 1;
-        switch (x) {
-            case 1:
-                System.out.println("X is " + x);
-                break;
-            default:
-                System.out.println("X is " + x);
-                break;
-        }
+        System.out.println();
     }
 }
