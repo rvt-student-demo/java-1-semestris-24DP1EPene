fe7cc1c8e50cda14e380068fd0c9db984d2099da
Emīls Penesis <A240733EP@rvt.lv>
2025-09-24 09:42:03 +0300

    Delikateses

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,36 +1,17 @@
 package rvt;
+
 import java.util.Scanner;
 
-    public class App {
-        public static void main(String[] args) {
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the value of the gift: ");
-        double gift = Double.valueOf(scanner.nextLine());
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        }
-        else {
-            System.out.print("Tax: ");
-        }
-        if (gift >= 5000 && gift <= 25000) {
-            System.out.println(100 + (gift - 5000) * 0.08);
-        }
-        else if (gift > 25000 && gift <= 55000) {
-            System.out.print(1700 + (gift - 25000) * 0.10);
-        }
-        else if (gift > 55000 && gift <= 200000) {
-            System.out.print(4700 + (gift - 55000) * 0.12);
-        }
-        else if (gift > 200000 && gift <= 1000000) {
-            System.out.print(22100 + (gift - 200000) * 0.15);
-        }
-        else if (gift > 1000000) {
-            System.out.print(142100 + (gift - 1000000) * 0.17);
-        }
-
-
-
+        System.out.print("Enter the product: ");
+        String product = scanner.nextLine();
+        System.out.print("Enter the price of the product: ");
+        Double price = Double.valueOf(scanner.nextLine());
+        System.out.print("Use express delivery? [1/0] ");
+        double delivery = ((Integer.valueOf(scanner.nextLine()) == 1 ? true : false) ? 3 + (price >= 10 ? -3 : 2) : 0);
         scanner.close();
-
+        System.out.println(String.format("Check: \n%s:%f\nDelivery: %f\nFull price: %f", product, price, delivery, delivery + price));
     }
 }
