c5c334e7708b2aefb05bf024f242cb352f6985d0
Emīls Penesis <A240733EP@rvt.lv>
2025-09-24 10:06:49 +0300

    Detaļas

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the product: ");
-        String product = scanner.nextLine();
-        System.out.print("Enter the price of the product: ");
-        Double price = Double.valueOf(scanner.nextLine());
-        System.out.print("Use express delivery? [1/0] ");
-        double delivery = ((Integer.valueOf(scanner.nextLine()) == 1 ? true : false) ? 3 + (price >= 10 ? -3 : 2) : 0);
+        System.out.print("Enter screw amount: ");
+        int screws = Integer.valueOf(scanner.nextLine());
+        System.out.print("Enter nut amount: ");
+        int nuts = Integer.valueOf(scanner.nextLine());
+        System.out.print("Enter plate amount: ");
+        int plates = Integer.valueOf(scanner.nextLine());
         scanner.close();
-        System.out.println(String.format("Check: \n%s:%f\nDelivery: %f\nFull price: %f", product, price, delivery, delivery + price));
+        System.out.println(String.format("Screw amount: %d\nNuts amount: %d\nPlates amount: %d\nCheck: %s", screws, nuts, plates, nuts == screws ? (nuts * 2 == plates ? "No errors!" : "Not enough plates!") : nuts > screws ? "Not enough screws" : "Not enough nuts"));
     }
 }
