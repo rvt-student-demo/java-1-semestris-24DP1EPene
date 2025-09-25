3cc7a4bf724d24f897a44173975657c8a5932e66
Emīls Penesis <A240733EP@rvt.lv>
2025-09-25 15:29:02 +0300

    Number and sum of numbers

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter screw amount: ");
-        int screws = Integer.valueOf(scanner.nextLine());
-        System.out.print("Enter nut amount: ");
-        int nuts = Integer.valueOf(scanner.nextLine());
-        System.out.print("Enter plate amount: ");
-        int plates = Integer.valueOf(scanner.nextLine());
+
+        int count = 0;
+        int sum = 0;
+        for (int input = -1; input != 0;) {
+            System.out.print(String.format("Enter the %d number: ", count));
+            input = Integer.valueOf(scanner.nextLine());
+            count++;
+            sum += input;
+        }
+        System.out.println(String.format("Entered %d numbers\nThe sum of those numbers is %d", count, sum));
         scanner.close();
-        System.out.println(String.format("Screw amount: %d\nNuts amount: %d\nPlates amount: %d\nCheck: %s", screws, nuts, plates, nuts == screws ? (nuts * 2 == plates ? "No errors!" : "Not enough plates!") : nuts > screws ? "Not enough screws" : "Not enough nuts"));
     }
 }
