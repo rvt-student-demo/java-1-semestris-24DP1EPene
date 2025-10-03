8a297391c406f9c85db8bad8bd51134752dbf613
EMILIO <emils.nopietnais@gmail.com>
2025-10-03 09:27:43 +0000

    spēle

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,17 +1,32 @@
 package rvt;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
-    public static void divisibleByThreeInRange(int start, int end) {
-        if (start == 0) {
-            start++;
+    public static void guessing_game(Scanner scanner) {
+        Random random = new Random();
+        int pc_pick = random.nextInt(10) + 1;
+        boolean won = false;
+
+        System.out.println("Try to guess a number the computer picked from 1-10, inclusive.");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Guess: ");
+            if (scanner.nextInt() == pc_pick) {
+                System.out.println("You won!");
+                won = true;
+                break;
+            }
+            else {
+                System.out.println("Wrong guess!\nTry again.");
+            }
         }
-        for (int i = start; i < end + 1; i++) {
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }        
+        if (!won) {
+            System.out.println("You lost!");
         }
-    }
+    }   
     public static void main(String[] args) {
-        divisibleByThreeInRange(2, 10);
+        Scanner scanner = new Scanner(System.in);
+        guessing_game(scanner);
     }
 }
