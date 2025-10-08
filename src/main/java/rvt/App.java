4928738247777176a823e8ba30ea962e55e0ad91
Emīls Penesis <A240733EP@rvt.lv>
2025-10-08 11:46:52 +0300

    swap

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,32 +1,35 @@
 package rvt;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    public static void guessing_game(Scanner scanner) {
-        Random random = new Random();
-        int pc_pick = random.nextInt(10) + 1;
-        boolean won = false;
-
-        System.out.println("Try to guess a number the computer picked from 1-10, inclusive.");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Guess: ");
-            if (scanner.nextInt() == pc_pick) {
-                System.out.println("You won!");
-                won = true;
-                break;
-            }
-            else {
-                System.out.println("Wrong guess!\nTry again.");
-            }
-        }
-        if (!won) {
-            System.out.println("You lost!");
-        }
-    }   
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        guessing_game(scanner);
+
+        int[] numbers = new int[4];
+        numbers[0] = 42;
+        numbers[1] = 13;
+        numbers[2] = 12;
+        numbers[3] = 7;
+
+        int index = 0;
+        while (index < numbers.length) {
+            System.out.println(numbers[index]);
+            index = index + 1;
+        }
+        int index_1 = scanner.nextInt();
+        int index_2 = scanner.nextInt();
+        int value_1 = numbers[index_1];
+        int value_2 = numbers[index_2];
+        numbers[index_1] = value_2;
+        numbers[index_2] = value_1;
+
+        index = 0;
+        while (index < numbers.length) {
+            System.out.println(numbers[index]);
+            index = index + 1;
+        }
+        scanner.close();
     }
 }
