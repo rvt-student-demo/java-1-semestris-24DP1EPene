e696f0b7402ea22eb69c9ea02f4e46a1ffb51f9b
Emīls Penesis <A240733EP@rvt.lv>
2025-10-08 11:52:05 +0300

    printer

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,35 +1,14 @@
 package rvt;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        int[] numbers = new int[4];
-        numbers[0] = 42;
-        numbers[1] = 13;
-        numbers[2] = 12;
-        numbers[3] = 7;
-
-        int index = 0;
-        while (index < numbers.length) {
-            System.out.println(numbers[index]);
-            index = index + 1;
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+        
+    }
+    public static void printArrayInStars(int[] array){
+        for(int i = 0; i < array.length; i ++){
+            System.out.println("*".repeat(array[i]));
         }
-        int index_1 = scanner.nextInt();
-        int index_2 = scanner.nextInt();
-        int value_1 = numbers[index_1];
-        int value_2 = numbers[index_2];
-        numbers[index_1] = value_2;
-        numbers[index_2] = value_1;
-
-        index = 0;
-        while (index < numbers.length) {
-            System.out.println(numbers[index]);
-            index = index + 1;
-        }
-        scanner.close();
     }
 }
