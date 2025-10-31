502c0229f95e062f0fe4c3cddfafcd7eac9712de
EMILIO <emils.nopietnais@gmail.com>
2025-10-31 16:26:51 +0000

    Completed payment card excercise on java-mooc couse.

---
+++ src/main/java/rvt/PaymentCard.java
@@ -0,0 +1,35 @@
+package rvt;
+
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        balance = openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + balance + " euros";
+    }
+
+    public void eatAffordably() {
+        if (balance >= 2.6) {
+            balance -= 2.6;
+        }
+    }
+
+    public void eatHeartily() {
+        if (balance >= 4.6) {
+            balance -= 4.6;
+        }
+    }
+
+    public void addMoney(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+
+        if (balance > 150) {
+            balance = 150;
+        }
+    }
+}
