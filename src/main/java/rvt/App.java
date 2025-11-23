f8f2cbc6f0b84b3ed671c4a5f9ec6be218919ec9
EMILIO <emils.nopietnais@gmail.com>
2025-11-23 20:51:02 +0000

    Books

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,17 +2,31 @@ package rvt;
 
 public class App {
     public static void main(String[] args) {
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
-        System.out.println(String.format("Paul: %s\nMatt: %s", paulsCard.toString(), mattsCard.toString()));
-        paulsCard.addMoney(20);
-        mattsCard.eatHeartily();
-        System.out.println(String.format("Paul: %s\nMatt: %s", paulsCard.toString(), mattsCard.toString()));
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
-        System.out.println(String.format("Paul: %s\nMatt: %s", paulsCard.toString(), mattsCard.toString()));
+        Custom_input input = new Custom_input();
+
+        int book_count = input.input("Enter the book's count: ", "int");
+        Book[] books = new Book[book_count];
+
+        for (int i = 0; i < book_count; i++) {
+            System.out.println("Filling out " + (i + 1) + ". book's info.");
+            books[i] = new Book(input.input("Enter the release year: ", "int"), input.input("Enter the name of the title: "), input.input("Enter the page count: ", "int"));
+        }
+        String info = input.input("Enter the info you wish to see: ");
+        for (int i = 0; i < book_count; i++) {
+            switch (info) {
+                case "everything":
+                    System.out.printf("%d. books info: \nThe Title of the book: %s\nThe pages count: %d\nThe release year: %d\n", i + 1, books[i].title, books[i].pages_count, books[i].year);
+                    break;
+                    case "name":
+                    System.out.printf("%d. books info: \nThe Title of the book: %s\n", i + 1, books[i].title);
+                    break;
+                case "page count":
+                    System.out.printf("%d. books info: \nThe pages count: %d\n", i + 1, books[i].pages_count);
+                    break;
+                case "release year":
+                    System.out.printf("%d. books info: \nThe release year: %d\n", i + 1, books[i].year);
+                    break;
+            }
+        }
     }
 }
